f02c1efd9820bdf0ce1e7cc4458e3225631f5a5b
Yanik Kendler <70104756+YanikKendler@users.noreply.github.com>
2026-01-21 15:27:59 +0530

    added cookie svg to cookie page

--- frontend/src/app/legal/cookies/page.tsx
+++ frontend/src/app/legal/cookies/page.tsx
@@ -5,6 +5,35 @@ export default function Cookies(){
             <p>Shotly only uses technically necessary cookies to ensure the basic functionality of the web application
                 (e.g., for login and session management). No tracking or marketing cookies are used.</p>
             <p>By using Shotly, you agree to the use of these necessary cookies. Separate consent is not required.</p>
+            <div className="cookie" style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                marginTop: "4rem"
+            }}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    viewBox="0 0 110.0 100.0"
+                    fill={"var(--text-70)"}
+                    style={{
+                        height: "25vw",
+                    }}
+                >
+                    <path d="m90.961 45.359c-0.14063-1.1602-1.1875-1.9023-2.3164-1.5938-2.9531 0.80859-6.2344 0.074219-8.5547-2.2461-0.73047-0.73047-1.2969-1.5586-1.7109-2.4375-0.42969-0.91406-1.5547-1.1953-2.4805-0.79297-3.9453 1.7188-8.7031 0.97266-11.93-2.2539-3.2266-3.2266-3.9727-7.9844-2.2539-11.93 0.40234-0.92578 0.12109-2.0508-0.79297-2.4805-0.88281-0.41406-1.7109-0.98047-2.4375-1.7109-2.3203-2.3203-3.0547-5.6016-2.2461-8.5547 0.30859-1.1289-0.43359-2.1758-1.5938-2.3164-1.5234-0.18359-3.0703-0.28906-4.6406-0.28906-23.656-0.003906-42.66 19.902-41.172 43.879 1.2734 20.543 17.992 37.262 38.535 38.535 23.973 1.4883 43.883-17.516 43.883-41.168 0-1.5742-0.10547-3.1172-0.28906-4.6406zm-62.523 17.766c-2.5898 0-4.6875-2.0977-4.6875-4.6875s2.0977-4.6875 4.6875-4.6875 4.6875 2.0977 4.6875 4.6875-2.0977 4.6875-4.6875 4.6875zm10.312-28.125c-3.1055 0-5.625-2.5195-5.625-5.625s2.5195-5.625 5.625-5.625 5.625 2.5195 5.625 5.625-2.5195 5.625-5.625 5.625zm10.312 16.875c-2.5898 0-4.6875-2.0977-4.6875-4.6875s2.0977-4.6875 4.6875-4.6875 4.6875 2.0977 4.6875 4.6875-2.0977 4.6875-4.6875 4.6875zm6.5625 24.375c-3.1055 0-5.625-2.5195-5.625-5.625s2.5195-5.625 5.625-5.625 5.625 2.5195 5.625 5.625-2.5195 5.625-5.625 5.625zm15.938-15c-2.5898 0-4.6875-2.0977-4.6875-4.6875s2.0977-4.6875 4.6875-4.6875 4.6875 2.0977 4.6875 4.6875-2.0977 4.6875-4.6875 4.6875z"/>
+                    <path d="m76.25 25.625c0 5-7.5 5-7.5 0s7.5-5 7.5 0z"/>
+                    <path d="m87.5 30.312c0 3.75-5.625 3.75-5.625 0s5.625-3.75 5.625 0z"/>
+                    <path d="m80 15.312c0 3.75-5.625 3.75-5.625 0s5.625-3.75 5.625 0z"/>
+                </svg>
+                <p style={{
+                    fontSize: "0.8rem",
+                    color: "var(--text-60)",
+                    textAlign: "center"
+                }}>
+                    Cookie by Hilmy Abiyyu Asad from <a className={"inline"} style={{color: "var(--text-50)"}} href="https://thenounproject.com/browse/icons/term/cookie/" target="_blank" title="cookie Icons">Noun Project</a> (CC BY 3.0)<br/>
+                    Cookie added for <a className={"inline"} style={{color: "var(--text-50)"}} href="https://https://michael.leisch.me/">Michael Leisch</a> from Austria :3
+                </p>
+            </div>
         </>
     )
 }
