5bb79a40f9804d1d3006a693f52c9d862391b1b9
Yanik Kendler <70104756+YanikKendler@users.noreply.github.com>
2026-04-08 19:21:11 +0530

    updated backend Dockerfile

--- backend/build.cmd
+++ backend/build.cmd
@@ -5,19 +5,6 @@ if "%VERSION%"=="" echo VERSION NAME NOT PROVIDED & exit /b 1
 
 set IMAGE=yanikkendler/shotly-backend:%VERSION%
 
-REM Maven (verwende mvnw.cmd auf Windows falls vorhanden)
-if exist mvnw.cmd (
-    call mvnw.cmd clean package || (
-        echo MAVEN BUILD FAILED
-        exit /b 0
-    )
-) else (
-    .\mvnw clean package || (
-        echo MAVEN BUILD FAILED
-        exit /b 0
-    )
-)
-
 docker build -t %IMAGE% . || (
     echo DOCKER BUILD FAILED
     exit /b 0
