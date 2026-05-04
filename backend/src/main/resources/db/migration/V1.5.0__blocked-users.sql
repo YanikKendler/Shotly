3a90b0da3d8a4fe8948bca5c1c174241517ed454
Yanik Kendler <70104756+YanikKendler@users.noreply.github.com>
2026-05-04 12:44:23 +0530

    removed unneeded build files, fixed #6 flyway sql

--- backend/src/main/resources/db/migration/V1.5.0__blocked-users.sql
+++ backend/src/main/resources/db/migration/V1.5.0__blocked-users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE app_user_blocked_user (
-    user_id bigint NOT NULL,
-    blocked_user_id bigint NOT NULL,
+    user_id UUID NOT NULL,
+    blocked_user_id UUID NOT NULL,
     PRIMARY KEY (user_id, blocked_user_id),
     CONSTRAINT fk_app_user_blocked_user_user
         FOREIGN KEY (user_id) REFERENCES app_user(id),
