4cb51274e710c655b19a4bcd9360b12eca23a821
Yanik Kendler <70104756+YanikKendler@users.noreply.github.com>
2026-07-19 19:37:00 +0600

    dashboard floaters mobile version

--- frontend/src/components/app/dashboard/dashboardDialogFloater/dashboardDialogFloater.tsx
+++ frontend/src/components/app/dashboard/dashboardDialogFloater/dashboardDialogFloater.tsx
@@ -9,22 +9,10 @@ export default function DashboardDialogFloater({
     children: ReactNode
     visible: boolean
 }){
-    const isDialogMode = window.innerWidth < 500
-
-    const dialogRef = useRef<DialogRef>(null);
-
-    useEffect(() => {
-        dialogRef.current?.setOpen(visible);
-    }, [visible]);
-
-    if(isDialogMode)
-        return <Dialog ref={dialogRef} defaultOpen={visible} contentClassName={"dashboardDialogFloater dialogMode"}>
-            {children}
-        </Dialog>
-
-    return (
-        <div className={"dashboardDialogFloater floating"}>
-            {children}
-        </div>
-    )
+    if(visible)
+        return (
+            <div className={"dashboardDialogFloater floating"}>
+                {children}
+            </div>
+        )
 }
